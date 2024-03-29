4322cf77b3da37827b6546ad83eda392c3f1ab54
Soroush <ranjbar.soroush32@gmail.com>
2024-03-29 22:20:18 +0430

    Footer Responsive designed

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -12,9 +12,9 @@ import senfIcon from "../../public/images/Footer/senf.png";
 const Footer = () => {
   return (
     <div className="p-10">
-      <div className="flex justify-between">
-        <div className="flex justify-between">
-          <div className="p-2">
+      <div className="flex flex-col md:flex-row my-2 md:my-0 justify-between ">
+        <div className="flex justify-between my-4 md:my-0">
+          <div className="md:p-2">
             <Image src={snappIcon} className="pt-1" />
           </div>
           <div className="flex flex-col">
@@ -58,41 +58,43 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col text-xs text-gray-700">
-          <Link className="py-2" href={""}>
-            درباره اسنپ‌فود
-          </Link>
-          <Link className="py-2" href={""}>
-            فرصت‌های شغلی
-          </Link>
-          <Link className="py-2" href={""}>
-            وبلاگ
-          </Link>
-          <Link className="py-2" href={""}>
-            قوانین سایت
-          </Link>
-          <Link className="py-2" href={""}>
-            حریم خصوصی
-          </Link>
-          <Link className="py-2" href={""}>
-            ثبت‌نام فروشندگان
-          </Link>
+        <div className="flex justify-between md:w-1/3">
+          <div className="flex flex-col text-xs text-gray-700">
+            <Link className="py-2" href={""}>
+              درباره اسنپ‌فود
+            </Link>
+            <Link className="py-2" href={""}>
+              فرصت‌های شغلی
+            </Link>
+            <Link className="py-2" href={""}>
+              وبلاگ
+            </Link>
+            <Link className="py-2" href={""}>
+              قوانین سایت
+            </Link>
+            <Link className="py-2" href={""}>
+              حریم خصوصی
+            </Link>
+            <Link className="py-2" href={""}>
+              ثبت‌نام فروشندگان
+            </Link>
+          </div>
+          <div className="flex flex-col text-xs text-gray-700">
+            <Link className="py-2" href={""}>
+              تماس با اسنپ‌فود
+            </Link>
+            <Link className="py-2" href={""}>
+              پرسش‌های متداول
+            </Link>
+            <Link className="py-2" href={""}>
+              ثبت شکایت
+            </Link>
+            <Link className="py-2" href={""}>
+              اپلیکیشن موبایل
+            </Link>
+          </div>
         </div>
-        <div className="flex flex-col text-xs text-gray-700">
-          <Link className="py-2" href={""}>
-            تماس با اسنپ‌فود
-          </Link>
-          <Link className="py-2" href={""}>
-            پرسش‌های متداول
-          </Link>
-          <Link className="py-2" href={""}>
-            ثبت شکایت
-          </Link>
-          <Link className="py-2" href={""}>
-            اپلیکیشن موبایل
-          </Link>
-        </div>
-        <div>
+        <div className="flex justify-end md:block">
           <Image src={senfIcon} />
         </div>
       </div>
