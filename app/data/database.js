63840029f75e704f2fb81711eb29e9ad3830b1c6
Soroush <ranjbar.soroush32@gmail.com>
2024-04-06 15:03:08 +0430

    Some Product Added to database

--- app/data/database.js
+++ app/data/database.js
@@ -2,12 +2,12 @@ import KetringLogo from "../../public/images/Restaurants/Ketring-logo.jpg";
 import KetringBackground from "../../public/images/Restaurants/Ketring-background.jpg";
 import PalestineLogo from "../../public/images/Restaurants/Palestine-logo.jpg";
 import PalestineBackground from "../../public/images/Restaurants/Palestine-background.jpeg";
-import ParastoLogo from "../../public/images/Restaurants/Parasto-logo.jpg";
-import ParastoBackground from "../../public/images/Restaurants/Parasto-background.jpg";
 import PashaLogo from "../../public/images/Restaurants/Pasha-logo.jpg";
 import PashaBackground from "../../public/images/Restaurants/Pasha-background.jpg";
 import ShilaLogo from "../../public/images/Restaurants/Shila-logo.jpg";
 import ShilaBackground from "../../public/images/Restaurants/Shila-background.jpg";
+import SalizLogo from "../../public/images/Restaurants/Saliz-logo.jpeg";
+import SalizBackground from "../../public/images/Restaurants/Saliz-background.jpg";
 
 import JojeKetring from "../../public/images/Foods/Ketring_Joje.jpeg";
 import KobidehKetring from "../../public/images/Foods/Ketring_Kobideh.jpeg";
@@ -22,7 +22,22 @@ import PalestineMasalaMorgh1 from "../../public/images/Foods/Palestine_MasalaMor
 import PalestineSalad from "../../public/images/Foods/Palestine_Salad.jpg";
 import PalestineKotlet from "../../public/images/Foods/Palestine_Kotlet.jpg";
 import Palestine_Kotlet1 from "../../public/images/Foods/Palestine_Kotlet_1.jpg";
-import Palestine_Yatimche from "../../public/images/Foods/Palestine_Kotlet_1.jpg";
+import Palestine_Yatimche from "../../public/images/Foods/Palestine_Yatimche.jpg";
+import Shila_ChickenCheeze from "../../public/images/Foods/Shila_ChickenCheeze.jpg";
+import Shila_Chicken from "../../public/images/Foods/Shila_Chicken.jpg";
+import Shila_Mix from "../../public/images/Foods/Shila_Mix.jpg";
+import Shila_Morgh from "../../public/images/Foods/Shila_Morgh.jpg";
+import Shila_Peperoni from "../../public/images/Foods/Shila_Peperoni.jpg";
+import Pasha_Gheimeh from "../../public/images/Foods/Pasha_Gheimeh.jpeg";
+import Pasha_Ghormeh from "../../public/images/Foods/Pasha_Ghormeh.jpeg";
+import Pasha_Kobideh from "../../public/images/Foods/Pasha_Kobideh.jpeg";
+import Pasha_Vaziri from "../../public/images/Foods/Pasha_Vaziri.jpeg";
+import Pasha_Zereshk from "../../public/images/Foods/Pasha_Zereshk.jpeg";
+import Saliz_Burger from "../../public/images/Foods/Saliz_Burger.jpg";
+import Saliz_Mashrom from "../../public/images/Foods/Saliz_Mashrom.jpg";
+import Saliz_Zhambon from "../../public/images/Foods/Saliz_Zhambon.jpg";
+import Saliz_ZhambonMix from "../../public/images/Foods/Saliz_ZhambonMix.jpg";
+import Saliz_Special from "../../public/images/Foods/Saliz_Special.jpeg";
 
 import irani from "../../public/images/All_Food_Category/Irani.jpg";
 import fastfood from "../../public/images/All_Food_Category/fastfood.jpg";
@@ -219,9 +234,187 @@ export const PRODUCTS = [
     price: 115400,
     main_img: Palestine_Yatimche,
     other_img: [Palestine_Yatimche, PalestinePackage],
-    rate: "4.1",
+    rate: "4.7",
     restaurant_id: 2,
     category: IRANI,
+  },
+  {
+    id: 13,
+    title: "پیتزا فیتکس مخلوط",
+    description:
+      "ژامبون گوشت و مرغ، هات داگ فرانکفورتر، قارچ، زیتون، گوجه، فلفل دلمه ای، پنیر موزارلا، سس مخصوص، سیب زمینی سرخ شده",
+    price: 144000,
+    main_img: Shila_Mix,
+    other_img: "",
+    rate: "4.7",
+    restaurant_id: 5,
+    category: PIZZA,
+  },
+  {
+    id: 14,
+    title: "چیکن چیز کلاسیک",
+    description: "مرغ سوخاری، پنیر پیتزا، سیب زمینی سرخ شده، سس مخصوص",
+    price: 216000,
+    main_img: Shila_ChickenCheeze,
+    other_img: "",
+    rate: "4.7",
+    restaurant_id: 5,
+    category: SOKHARI,
+  },
+  {
+    id: 15,
+    title: "پیتزا فیتکس مرغ و شوید",
+    description:
+      "سینه مرغ گریل شده، قارچ، شوید، زیتون، جعفری، پنیر موزارلا، سس مخصوص، سیب زمینی سرخ شده",
+    price: 158000,
+    main_img: Shila_Morgh,
+    other_img: "",
+    rate: "4.7",
+    restaurant_id: 5,
+    category: PIZZA,
+  },
+  {
+    id: 16,
+    title: "پیتزا فیتکس پپرونی",
+    description:
+      "کالباس پپرونی، قارچ، پنیر موزارلا، سس مخصوص، سیب زمینی سرخ شده",
+    price: 164000,
+    main_img: Shila_Peperoni,
+    other_img: "",
+    rate: "4.7",
+    restaurant_id: 5,
+    category: PIZZA,
+  },
+  {
+    id: 17,
+    title: "چیکن استریپس",
+    description:
+      "۳ تکه فیله مرغ سوخاری، سس مخصوص سیر، سس مخصوص چیلی، سالاد کلم، سیب زمینی سرخ شده، نان بروتچن",
+    price: 252000,
+    main_img: Shila_Chicken,
+    other_img: "",
+    rate: "4.7",
+    restaurant_id: 5,
+    category: PIZZA,
+  },
+  {
+    id: 18,
+    title: "چلو کباب کوبیده یک سیخ",
+    description:
+      "کباب کوبیده گوشت مخلوط گوساله و گوسفندی ۱۰۰ گرمی، ۳۵۰ گرم برنج ایرانی",
+    price: 95000,
+    main_img: Pasha_Kobideh,
+    other_img: "",
+    rate: "4.4",
+    restaurant_id: 4,
+    category: KEBAB,
+  },
+  {
+    id: 19,
+    title: "چلو کباب وزیری",
+    description:
+      "یک سیخ کباب کوبیده گوشت مخلوط گوساله و گوسفندی ۱۰۰ گرمی، یک سیخ جوجه کباب سینه مرغ ۱۵۰ گرمی، ۳۵۰ گرم برنج ایرانی",
+    price: 142500,
+    main_img: Pasha_Vaziri,
+    other_img: "",
+    rate: "4.1",
+    restaurant_id: 4,
+    category: KEBAB,
+  },
+  {
+    id: 20,
+    title: "زرشک پلو با مرغ سس پز ران",
+    description: "مرغ سس پز ۳۵۰ گرمی، ۳۵۰ گرم برنج ایرانی",
+    price: 115000,
+    main_img: Pasha_Zereshk,
+    other_img: "",
+    rate: "4.6",
+    restaurant_id: 4,
+    category: IRANI,
+  },
+  {
+    id: 21,
+    title: "چلو خورشت قیمه سیب زمینی",
+    description:
+      "۳۰۰ گرم خورشت قیمه سیب زمینی، ۵۰ گرم گوشت گوساله، سیب زمینی سرخ شده، ۳۵۰ گرم برنج ایرانی",
+    price: 80000,
+    main_img: Pasha_Gheimeh,
+    other_img: "",
+    rate: "4.3",
+    restaurant_id: 4,
+    category: IRANI,
+  },
+  {
+    id: 22,
+    title: "چلو خورشت قورمه سبزی",
+    description:
+      "۳۰۰ گرم خورشت قورمه سبزی، ۵۰ گرم گوشت گوساله، ۳۵۰ گرم برنج ایرانی",
+    price: 80000,
+    main_img: Pasha_Ghormeh,
+    other_img: "",
+    rate: "4.4",
+    restaurant_id: 4,
+    category: IRANI,
+  },
+  {
+    id: 23,
+    title: "پیتزا مخصوص آمریکایی",
+    description:
+      "خمیر پیتزا آمریکایی، سس مخصوص، پنیر پیتزا، ژامبون گوشت و مرغ ۹۰%، قارچ، فلفل دلمه ای، ذرت",
+    price: 180000,
+    main_img: Saliz_Special,
+    other_img: "",
+    rate: "4.0",
+    restaurant_id: 3,
+    category: PIZZA,
+  },
+  {
+    id: 24,
+    title: "برگر",
+    description:
+      "یک عدد برگر دستی ۹۰٪ گوشت گوساله ۱۶۰ گرمی، کاهو، گوجه، خیارشور، سس مخصوص، نان باگت",
+    price: 165000,
+    main_img: Saliz_Burger,
+    other_img: "",
+    rate: "4.6",
+    restaurant_id: 3,
+    category: BURGER,
+  },
+  {
+    id: 25,
+    title: "ماشروم برگر",
+    description:
+      "یک عدد برگر دستی ۹۰% گوشت گوسالە ۱۶۰ گرمی، ۱۰۰ گرم قارچ، کاھو، گوجە، خیارشور، سس مخصوص، نان باگت",
+    price: 185000,
+    main_img: Saliz_Mashrom,
+    other_img: "",
+    rate: "4.2",
+    restaurant_id: 3,
+    category: BURGER,
+  },
+  {
+    id: 26,
+    title: "ساندویچ ژامبون مرغ تنوری",
+    description:
+      "۱۳۰ گرم ژامبون مرغ ۹۰%، قارچ، پنیر، ذرت، کاهو، گوجه، خیارشور، سس مخصوص، نان باگت",
+    price: 165000,
+    main_img: Saliz_Zhambon,
+    other_img: "",
+    rate: "4.7",
+    restaurant_id: 3,
+    category: SANDWICH,
+  },
+  {
+    id: 27,
+    title: "ساندویچ ژامبون میکس سرد",
+    description:
+      "۱۳۰ گرم ژامبون گوشت و مرغ ۹۰%، کاهو، گوجه، خیارشور، خلال سیب زمینی، سس مایونز، نان باگت",
+    price: 130000,
+    main_img: Saliz_ZhambonMix,
+    other_img: "",
+    rate: "4.5",
+    restaurant_id: 3,
+    category: SANDWICH,
   },
 ];
 
@@ -244,16 +437,16 @@ export const RESTAURANTS = [
   },
   {
     id: 3,
-    name: "فست فود پرستو",
-    category: [FASTFOOD, SOKHARI],
-    logo: ParastoLogo,
-    background: ParastoBackground,
-    rate: "جدید",
+    name: "فست فود سالیز",
+    category: [SANDWICH, BURGER, PIZZA],
+    logo: SalizLogo,
+    background: SalizBackground,
+    rate: "4.4",
   },
   {
     id: 4,
     name: "کترینگ پاشا",
-    category: [IRANI, FASTFOOD, PASTA],
+    category: [IRANI, KEBAB],
     logo: PashaLogo,
     background: PashaBackground,
     rate: "جدید",
@@ -261,7 +454,7 @@ export const RESTAURANTS = [
   {
     id: 5,
     name: "پیتزا شیلا (حجاب)",
-    category: [FASTFOOD, PIZZA, SANDWICH],
+    category: [PIZZA, SOKHARI],
     logo: ShilaLogo,
     background: ShilaBackground,
     rate: 4.7,
